ae5f981bf2fc3ad5570a529c1b29fe87676a6aee
MostlyHarmless <sja4201@gmail.com>
2024-10-28 04:29:31 -0700

    Content warnings are good

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,10 @@
         <br/>
         <h3>The last thing you remember is a psychiatrist's office.</h3>
         <br/>
+        <h4>Content warning: This is a game about mental health and social media, presented through short form video.
+            The worst of the internet is on display in this game. None of the opinions presented within are endorsed
+            by the creator.</h4>
+        <br/>
         <a href="/live"><h3>Begin.</h3></a>
     </div>
 </main>
@@ -15,5 +19,12 @@
 <style>
     .intro {
         text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    h4 {
+        width: 40%;
+        font-weight: lighter;
     }
 </style>
